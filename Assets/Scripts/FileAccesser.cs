f7b847854a4d157ba40d90b79e1bee6be6ecf322
Ayush Shekhar <ay.shekhar01@gmail.com>
2022-09-15 12:10:22 -0600

    all artifacts

---
+++ Assets/Scripts/FileAccesser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class FileAccesser : MonoBehaviour
+{
+    [MenuItem("Upload .stl file")]
+    public void Apply()
+    {
+        Texture2D texture = Selection.activeObject as Texture2D;
+        string path = EditorUtility.OpenFilePanel("Overwrite with stl", "", "stl");
+        if (path.Length != 0)
+        {
+            var fileContent = File.ReadAllBytes(path);
+            texture.LoadImage(fileContent);
+        }
+
+
+    }
+}
